40cd16fb10542bd019a0c2235dc145c978141547
Habeeb Hooshmand <habeeb.hooshmand@gmail.com>
2025-01-08 23:52:46 -0600

    Add custom font and restructure App.js components for better organization

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
           <Box
             sx={{
               width: '100vw',
-              height: '100vh',
+              height: '96vh',
               backgroundImage: 'url(/assets/controller.webp), url(/assets/controller.png)',
               backgroundSize: 'cover',
               backgroundPosition: 'center',
@@ -51,112 +51,117 @@ const App = () => {
           </Box>
         </header>
         <article className='App-body'>
-          <section style={{ textAlign: 'center', marginTop: '20px' }}>
-            <h2 style={{ color: 'white' }}>Latest Set</h2>
-            <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', gap: '20px' }}>
-              <iframe 
-                title="Latest Sets" 
-                width="100%" 
-                height="300" 
-                scrolling="no" 
-                frameBorder="no" 
-                allow="autoplay" 
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1941877247&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
-              </iframe>
-              <Button 
-                variant='text'
-                href="https://soundcloud.com/iamhabeeb/sets/my-sets?si=86c25d2d9cbd410599dc95be9dfb27bd&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
-                target="_blank" 
-                rel="noopener noreferrer" 
-                startIcon={<LibraryMusicIcon />}
-              >
-                All mixes
-              </Button>
-            </div>
-          </section>
-          <hr style={{ width: '90%', margin: '20px auto', border: '1px solid grey' }} />
-          <section style={{ textAlign: 'center', marginTop: '20px' }}>
-            <h2 style={{ color: 'white' }}>Instagram</h2>
-            <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', gap: '20px' }}>
-              <iframe 
-                title='Instagram Feed'
-                src="https://www.instagram.com/habeebmh_/embed" 
-                width="100%" 
-                height="500" 
-                frameBorder="0" 
-                scrolling="no" 
-                allowtransparency="true"
-              ></iframe>
-              <Button 
-                variant='text' 
-                startIcon={<InstagramIcon />} 
-                className='App-ig-button' 
-                href='https://www.instagram.com/habeebmh_/' 
-                target='_blank' 
-                rel='noopener noreferrer'
-              >Instagram</Button>
-            </div>
-          </section>
-          <hr style={{ width: '90%', margin: '20px auto', border: '1px solid grey' }} />
-          <section style={{ textAlign: 'center', marginTop: '20px' }}>
-            <h2 style={{ color: 'white' }}>Latest release</h2>
-            <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', gap: '20px' }}>
-              <iframe 
-                title="Latest Release" 
-                width="100%" 
-                height="300" 
-                scrolling="no" 
-                frameBorder="no" 
-                allow="autoplay" 
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1962120063&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-              ></iframe>
-              <Button 
-                variant='text' 
-                href='https://soundcloud.com/iamhabeeb' 
-                target='_blank' 
-                rel='noopener noreferrer'
-                startIcon={<CloudIcon />}
-              >
-                SoundCloud
-              </Button>
-            </div>
-          </section>
-          <section style={{ textAlign: 'center', marginTop: '20px' }}>
-            <h2 style={{ color: 'white' }}>Contact</h2>
-            <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', gap: '20px' }}>
-              <Button 
-                variant='text' 
-                href='mailto:habeeb@hooshmandenterprises.com'
-                startIcon={<EmailIcon />}
-              >
-                Email
-              </Button>
-            </div>
-          </section>
+          <hr />
+          <LatestRelease />
+          <hr />
+          <Instagram />
+          <hr />
+          <Contact />
+          <hr />
         </article>
-        <footer className='App-footer'>
-          <hr style={{ width: '90%', margin: '20px auto', border: '1px solid grey' }} />
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              gap: '20px',
-              padding: '20px',
-              backgroundColor: 'rgba(0,0,0,.5)'
-            }}
-          >
-            <Link href='https://www.instagram.com/habeebmh_/' target='_blank' rel='noopener noreferrer'>
-              <InstagramIcon sx={{ fontSize: 40 }} />
-            </Link>
-            <Link href='https://www.mixcloud.com/habeebmh/' target='_blank' rel='noopener noreferrer'>
-              <LibraryMusicIcon sx={{ fontSize: 40 }} />
-            </Link>
-          </Box>
-        </footer>
+        <Footer />
       </div>
     </ThemeProvider>
   )
 }
+
+const LatestRelease = () => (
+  <section style={{ textAlign: 'center' }}>
+    <h2 style={{ color: 'white' }}>Latest releases</h2>
+    <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', gap: '20px', marginBottom: '16px' }}>
+      <iframe 
+        title="Latest Release" 
+        width="100%" 
+        height="300" 
+        scrolling="no" 
+        frameBorder="no" 
+        allow="autoplay" 
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1962120063&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      ></iframe>
+    </div>
+    <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', gap: '20px' }}>
+      <iframe 
+        title="Latest Sets" 
+        width="100%" 
+        height="300" 
+        scrolling="no" 
+        frameBorder="no" 
+        allow="autoplay" 
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1941877247&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+      </iframe>
+      <Button 
+        variant='text' 
+        href='https://soundcloud.com/iamhabeeb' 
+        target='_blank' 
+        rel='noopener noreferrer'
+        startIcon={<CloudIcon />}
+      >
+        SoundCloud
+      </Button>
+    </div>
+  </section>
+)
+
+const Instagram = () => (
+  <section style={{ textAlign: 'center' }}>
+    <h2 style={{ color: 'white' }}>Instagram</h2>
+    <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', gap: '20px' }}>
+      <iframe 
+        title='Instagram Feed'
+        src="https://www.instagram.com/habeebmh_/embed" 
+        width="100%" 
+        height="500" 
+        frameBorder="0" 
+        scrolling="no" 
+        allowtransparency="true"
+      ></iframe>
+      <Button 
+        variant='text' 
+        startIcon={<InstagramIcon />} 
+        className='App-ig-button' 
+        href='https://www.instagram.com/habeebmh_/' 
+        target='_blank' 
+        rel='noopener noreferrer'
+      >Instagram</Button>
+    </div>
+  </section>
+)
+
+const Contact = () => (
+  <section style={{ textAlign: 'center' }}>
+    <h2 style={{ color: 'white' }}>Contact</h2>
+    <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', gap: '20px' }}>
+      <Button 
+        variant='text' 
+        href='mailto:habeeb@hooshmandenterprises.com'
+        startIcon={<EmailIcon />}
+      >
+        Email
+      </Button>
+    </div>
+  </section>
+)
+
+const Footer = () => (
+  <footer className='App-footer'>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '20px',
+        padding: '20px',
+        backgroundColor: 'rgba(0,0,0,.5)'
+      }}
+    >
+      <Link href='https://www.instagram.com/habeebmh_/' target='_blank' rel='noopener noreferrer'>
+        <InstagramIcon sx={{ fontSize: 40 }} />
+      </Link>
+      <Link href='https://www.mixcloud.com/habeebmh/' target='_blank' rel='noopener noreferrer'>
+        <LibraryMusicIcon sx={{ fontSize: 40 }} />
+      </Link>
+    </Box>
+  </footer>
+)
 
 export default App
